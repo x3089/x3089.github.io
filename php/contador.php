919495bd3cb653c1e5b3ed1720c0fa05f32ff58d
x3089 <astrapch4@gmail.com>
2025-04-09 17:04:33 +0200

    updated

--- php/contador.php
+++ php/contador.php
@@ -12,7 +12,7 @@ $visitas++;
 
 // Guarda el nuevo número en el archivo
 file_put_contents($archivo, $visitas);
-
+                                
 // Devuelve el número de visitas
 echo $visitas;
 ?>
